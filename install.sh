2f10cc2a1adca6332c6888269b0042d63139dbbb
Aarón <72323217+rmz01@users.noreply.github.com>
2024-09-30 10:57:39 +0000

    added install.sh and modified email in gitconfig

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -f /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ]; then
+    ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
+else
+    echo "Error: Archivo .gitconfig not found"
+fi
+
+
+gh alias set cd '!gh config set current-org "$1" 2>/dev/null'
+gh alias set pwd '!gh config get current-org'
+#gh extension install github/gh-classroom
+#gh extension install github/gh-copilot
+gh extension install gh-cli-for-education/gh-org-teams
+gh extension install crgue
