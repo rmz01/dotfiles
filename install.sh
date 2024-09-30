99c1f02e5d56659d8ab221a89c030d9342edd832
rmz01 <alu0101438238@ull.edu.es>
2024-09-30 11:13:46 +0000

    added extension install classroom and copilot

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 
 gh alias set cd '!gh config set current-org "$1" 2>/dev/null'
 gh alias set pwd '!gh config get current-org'
-#gh extension install github/gh-classroom
-#gh extension install github/gh-copilot
+gh extension install github/gh-classroom
+gh extension install github/gh-copilot
 gh extension install gh-cli-for-education/gh-org-teams
 gh extension install crguezl/gh-org-clone
